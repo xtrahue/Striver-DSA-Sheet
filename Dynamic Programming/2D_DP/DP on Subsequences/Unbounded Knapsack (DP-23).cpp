2c7ca35dea451d79c184249930d1a72990479edb
Indrajit Mondal <108055347+xtrahue@users.noreply.github.com>
2023-03-22 02:44:02 +0530

    Create Unbounded Knapsack (DP-23).cpp

---
+++ Dynamic Programming/2D_DP/DP on Subsequences/Unbounded Knapsack (DP-23).cpp
@@ -0,0 +1,54 @@
+//space optimized
+int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        //vector<vector<int>>dp(N,vector<int>(W+1,0)) ;
+        vector<int>prev(W+1);
+        vector<int>curr(W+1,0) ;
+        
+        for(int i=0;i<=W;i++){
+            prev[i]=(int)(i/wt[0])*val[0] ;
+        }
+        for(int i=1;i<N;i++){
+            for(int weight=0;weight<=W;weight++){
+                int nt=prev[weight] ;
+                int t=INT_MIN ;
+                if(wt[i]<=weight){
+                    t=val[i]+curr[weight-wt[i]] ;
+                }
+                
+                curr[weight]=max(t,nt) ;
+            }
+            prev=curr ;
+        }
+        return prev[W] ;
+    }
+
+
+
+//memoization
+
+class Solution{
+public:
+    int solveit(int ind,int W,int val[], int wt[],vector<vector<int>>&dp){
+        if(ind<=-1 || W==0){
+            return 0 ;
+        }
+        if(dp[ind][W]!=-1){
+            return dp[ind][W] ;
+        }
+        int nt=solveit(ind-1,W,val,wt,dp) ;
+        int t=INT_MIN ;
+        if(wt[ind]<=W){
+            t=val[ind]+solveit(ind,W-wt[ind],val,wt,dp) ;
+        }
+        
+        return dp[ind][W]=max(t,nt) ;
+    }
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        vector<vector<int>>dp(N,vector<int>(W+1,-1)) ;
+        return solveit(N-1,W,val,wt,dp) ;
+    }
+};
