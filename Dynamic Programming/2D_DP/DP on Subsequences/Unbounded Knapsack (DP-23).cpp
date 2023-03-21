2aa58c945b692b0f0cc6fd043b16d8018162fdf4
Indrajit Mondal <108055347+xtrahue@users.noreply.github.com>
2023-03-22 02:49:01 +0530

    Update Unbounded Knapsack (DP-23).cpp

--- Dynamic Programming/2D_DP/DP on Subsequences/Unbounded Knapsack (DP-23).cpp
+++ Dynamic Programming/2D_DP/DP on Subsequences/Unbounded Knapsack (DP-23).cpp
@@ -1,30 +1,3 @@
-//space optimized
-int knapSack(int N, int W, int val[], int wt[])
-    {
-        // code here
-        //vector<vector<int>>dp(N,vector<int>(W+1,0)) ;
-        vector<int>prev(W+1);
-        vector<int>curr(W+1,0) ;
-        
-        for(int i=0;i<=W;i++){
-            prev[i]=(int)(i/wt[0])*val[0] ;
-        }
-        for(int i=1;i<N;i++){
-            for(int weight=0;weight<=W;weight++){
-                int nt=prev[weight] ;
-                int t=INT_MIN ;
-                if(wt[i]<=weight){
-                    t=val[i]+curr[weight-wt[i]] ;
-                }
-                
-                curr[weight]=max(t,nt) ;
-            }
-            prev=curr ;
-        }
-        return prev[W] ;
-    }
-
-
 
 //memoization
 
@@ -52,3 +25,58 @@ public:
         return solveit(N-1,W,val,wt,dp) ;
     }
 };
+
+
+
+//space optimized
+int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        //vector<vector<int>>dp(N,vector<int>(W+1,0)) ;
+        vector<int>prev(W+1);
+        vector<int>curr(W+1,0) ;
+        
+        for(int i=0;i<=W;i++){
+            prev[i]=(int)(i/wt[0])*val[0] ;
+        }
+        for(int i=1;i<N;i++){
+            for(int weight=0;weight<=W;weight++){
+                int nt=prev[weight] ;
+                int t=INT_MIN ;
+                if(wt[i]<=weight){
+                    t=val[i]+curr[weight-wt[i]] ;
+                }
+                
+                curr[weight]=max(t,nt) ;
+            }
+            prev=curr ;
+        }
+        return prev[W] ;
+    }
+
+
+//space optimized using only prev
+int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        //vector<vector<int>>dp(N,vector<int>(W+1,0)) ;
+        vector<int>prev(W+1);
+        vector<int>curr(W+1,0) ;
+        
+        for(int i=0;i<=W;i++){
+            prev[i]=(int)(i/wt[0])*val[0] ;
+        }
+        for(int i=1;i<N;i++){
+            for(int weight=0;weight<=W;weight++){
+                int nt=prev[weight] ;
+                int t=INT_MIN ;
+                if(wt[i]<=weight){
+                    t=val[i]+prev[weight-wt[i]] ;
+                }
+                
+                prev[weight]=max(t,nt) ;
+            }
+            
+        }
+        return prev[W] ;
+    }
