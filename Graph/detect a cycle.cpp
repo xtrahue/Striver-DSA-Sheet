31b2086af112ce67f13ad3d5e1a0405f6ca8776f
Indrajit Mondal <108055347+xtrahue@users.noreply.github.com>
2023-08-12 23:07:43 +0530

    Update detect a cycle.cpp

--- Graph/detect a cycle.cpp
+++ Graph/detect a cycle.cpp
@@ -42,28 +42,6 @@ class Solution {
 //striver's approach by BFS
 class Solution {
   private:
-    bool solveit(int curr_node,int prev_node,int node,int cnt,vector<int>adj[]){
-        if(curr_node==node && cnt>2){
-            return 1 ;
-        }
-        
-        if(adj[curr_node].size()==1){
-            return 0;
-        }
-        
-        bool a=0 ;
-        
-        for(int i=0;i<adj[curr_node].size();i++){
-            if(adj[curr_node][i]!=prev_node){
-               a=a|| solveit(adj[curr_node][i],curr_node,node,cnt+1,adj) ;
-               if(a){
-                   return 1;
-               }
-            }
-        }
-        return a;
-    }
-    
     bool solveit(int src,vector<int>adj[],vector<bool>&vis){
         
         queue<pair<int,int>>qu ;
