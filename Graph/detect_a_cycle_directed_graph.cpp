dc3653f07e85823571ee38e56a0b7fa3c0fa49a8
Indrajit Mondal <108055347+xtrahue@users.noreply.github.com>
2023-08-17 00:00:47 +0530

    Update detect_a_cycle_directed_graph.cpp

--- Graph/detect_a_cycle_directed_graph.cpp
+++ Graph/detect_a_cycle_directed_graph.cpp
@@ -44,3 +44,43 @@ class Solution {
       return 0 ;
     }
 };
+
+
+//using khan's algorithm
+bool isCyclic(int n, vector<int> adj[]) {
+    vector<int> inDegree(n, 0); // Store in-degrees of nodes
+    queue<int> q;
+
+    // Calculate in-degrees
+    for (int i = 0; i < n; i++) {
+        for (int u : adj[i]) {
+            inDegree[u]++;
+        }
+    }
+
+    // Add nodes with in-degree 0 to the queue
+    for (int i = 0; i < n; i++) {
+        if (inDegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    int cnt = 0; // Count of visited nodes
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        cnt++;
+
+        // Reduce in-degrees of neighbors
+        for (int v : adj[u]) {
+            inDegree[v]--;
+            if (inDegree[v] == 0) {
+                q.push(v);
+            }
+        }
+    }
+
+    // If count of visited nodes is less than total nodes, cycle exists
+    return cnt != n;
+}
