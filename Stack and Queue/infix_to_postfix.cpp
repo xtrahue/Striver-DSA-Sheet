37c886594d44812f8381aa35120da015d2056691
Indrajit Mondal <108055347+xtrahue@users.noreply.github.com>
2024-01-13 17:13:57 +0530

    Create infix_to_postfix.cpp

---
+++ Stack and Queue/infix_to_postfix.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+
+int precedence(char x){
+    if(x=='^'){
+        return 3;
+    }
+    if(x=='*' || x=='/'){
+        return 2;
+    }
+    if(x=='+' || x=='-'){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+
+bool isLeft(char ch){
+    if(ch=='^'){
+        return 0;
+    }
+    return 1;
+}
+
+
+string inToPost(string &s){
+    stack<char>st ;
+    string res ;
+
+    for(int i=0;i<s.size();i++){
+        if(isalpha(s[i]) || isdigit(s[i])){
+            res.push_back(s[i]) ;
+        }
+        else if(s[i]=='('){
+            st.push('(') ;
+        }
+        else if(s[i]==')'){
+            while(st.top()!='('){
+                res.push_back(st.top()) ;
+                st.pop() ;
+            }
+            st.pop() ;
+        }
+        else{
+            while(!st.empty() && 
+            (precedence(s[i])<precedence(st.top()) || 
+            (precedence(s[i])==precedence(st.top()) && isLeft(s[i])))){
+                res.push_back(st.top()) ;
+                st.pop() ;
+            }
+            st.push(s[i]) ;
+        }
+    }
+
+    while(!st.empty()){
+        res.push_back(st.top()) ;
+        st.pop() ;
+    }
+
+    return res ;
+}
+
+
+int main()
+{
+    string s;
+    cin>>s ;
+    cout<<inToPost(s)<<endl ;
+}
